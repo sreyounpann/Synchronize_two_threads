c7aa25cc858765b0a0f8a1420ecc2d24fe56f0d6
User <User@MSI>
2023-08-20 10:49:28 +0700

    Add project files.

---
+++ Task2_FinalExam/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Task2_FinalExam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Cambria", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 23;
+            this.listBox1.Location = new System.Drawing.Point(129, 80);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(224, 326);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("Cambria", 10F, System.Drawing.FontStyle.Bold);
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 23;
+            this.listBox2.Location = new System.Drawing.Point(359, 80);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(224, 326);
+            this.listBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(192, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "THREAD 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cambria", 10F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(424, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "THREAD 2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 521);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Task2_FinalExam/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task2_FinalExam
+{
+    public partial class Form1 : Form
+    {
+        private AutoResetEvent thread1Event;
+        private AutoResetEvent thread2Event;
+        private int count;
+        private bool isIncrementing;
+        private bool isCountingBack;
+
+        public Form1()
+        {
+            InitializeComponent();
+            thread1Event = new AutoResetEvent(true);
+            thread2Event = new AutoResetEvent(false);
+            count = 1;
+            isIncrementing = true;
+            isCountingBack = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Task.Run(() => ThreadMethod1());
+            Task.Run(() => ThreadMethod2());
+        }
+
+        private void AppendText(ListBox listBox, string text)
+        {
+            if (listBox.InvokeRequired)
+            {
+                listBox.Invoke(new Action(() => AppendText(listBox, text)));
+            }
+            else
+            {
+                listBox.Items.Add(text);
+            }
+        }
+
+        private void ThreadMethod1()
+        {
+            while (true)
+            {
+                if (!isCountingBack)
+                {
+                    if (isIncrementing)
+                    {
+                        if (count <= 8)
+                        {
+                            thread1Event.WaitOne();
+                            Thread.Sleep(1000);
+                            listBox1.Invoke((Action)(() => AppendText(listBox1, count.ToString())));
+                            count++;
+                            thread2Event.Set();
+                        }
+                        else
+                        {
+                            isIncrementing = false;
+                            isCountingBack = true;
+                        }
+                    }
+                    else
+                    {
+                        if (count >= 1)
+                        {
+                            thread1Event.WaitOne();
+                            Thread.Sleep(1000);
+                            listBox1.Invoke((Action)(() => AppendText(listBox1, count.ToString())));
+                            count--;
+                            thread2Event.Set();
+                        }
+                        else
+                        {
+                            isIncrementing = true;
+                            thread2Event.Set();
+                        }
+                    }
+                }
+                else
+                {
+                    if (count >= 1)
+                    {
+                        thread1Event.WaitOne();
+                        Thread.Sleep(1000);
+                        listBox1.Invoke((Action)(() => AppendText(listBox1, count.ToString())));
+                        count--;
+                        thread2Event.Set();
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void ThreadMethod2()
+        {
+            while (true)
+            {
+                if (!isCountingBack)
+                {
+                    if (isIncrementing)
+                    {
+                        if (count <= 8)
+                        {
+                            thread2Event.WaitOne();
+                            Thread.Sleep(1000);
+                            listBox2.Invoke((Action)(() => AppendText(listBox2, count.ToString())));
+                            count++;
+                            thread1Event.Set();
+                        }
+                        else
+                        {
+                            isIncrementing = false;
+                            isCountingBack = true;
+                        }
+                    }
+                    else
+                    {
+                        if (count >= 1)
+                        {
+                            thread2Event.WaitOne();
+                            Thread.Sleep(1000);
+                            listBox2.Invoke((Action)(() => AppendText(listBox2, count.ToString())));
+                            count--;
+                            thread1Event.Set();
+                        }
+                        else
+                        {
+                            isIncrementing = true;
+                            thread1Event.Set();
+                        }
+                    }
+                }
+                else
+                {
+                    if (count >= 0)
+                    {
+                        thread2Event.WaitOne();
+                        Thread.Sleep(1000);
+                        listBox2.Invoke((Action)(() => AppendText(listBox2, count.ToString())));
+                        count--;
+                        thread1Event.Set();
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            thread1Event.Set();
+            thread2Event.Set();
+            base.OnFormClosing(e);
+        }
+    }
+}
